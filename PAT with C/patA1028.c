f8e5aeb1b2cb67e3b071b7df6e0cbb57eb44719b
4kingRAS <4king4ever@gmail.com>
2019-08-10 23:25:02 +0800

    Update patA1028.c

--- PAT with C/patA1028.c
+++ PAT with C/patA1028.c
@@ -1,16 +1,94 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ** PAT A 1028 List Sorting
+
+// 字符串，数字，特定数字格式 的排序
+// ascend, non-desc, non-desc
+#define MAXN 100001
+int type;
+ 
+struct stu
+{
+    int no;
+    char s[9];
+    int g;
+};
+
+int cmp(const void *a, const void *b)
+{
+    // default ascend , >:1, =:0 , <:-1
+    const struct stu *p = a;
+    const struct stu *q = b;
+    switch (type)
+    {
+    case 1:
+        return p->no - q->no;
+    case 2:
+        {
+            char a[9];
+            char b[9];
+            strcpy(a, p->s);
+            strcpy(b, q->s);
+            int la = strlen(a);
+            int lb = strlen(b);
+            for (int i = 0; (i < la|| i < lb); i++)
+            {
+                if (a[i] == b[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    return a[i] - b[i];
+                }
+            }
+
+            if (la == lb) 
+            {
+                return p->no - q->no;
+            }
+            else
+            {
+                return la - lb;
+            }
+        }  
+    default://case 3
+        {
+            if (p->g == q->g)
+            {
+                return p->no - q->no;
+            }
+            else
+            {
+                return p->g - q->g;
+            }
+        }
+    }
+}
 
 int main(void) {
-    int c1 = 0;
-    int c2 = 0;
-    scanf("%d %d", &c1, &c2);
+    int n;
+    
+    scanf("%d %d", &n, &type);
 
-    int h = (c2 - c1) / 360000;
-    int m = ((c2 - c1) / 6000) - h*60;
-    double s = ((c2 - c1) / 100.00) - h*3600 - m*60;
-    int ss = round(s);
+    struct stu a[n];
+    int no, g;
+    char str[9];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %s %d", &no, str, &g); //scanf 读字符串会补\0
+        a[i].no = no;
+        strcpy(a[i].s, str);
+        a[i].g = g;
+    }
+    qsort(a, n, sizeof(a[0]), cmp);
+    
+    for(int i = 0; i < n; ++i) 
+    {
+        printf("%06d %s %d\n", a[i].no, a[i].s, a[i].g);
+    }
 
-    printf("%02d:%02d:%02d\n", h, m, ss);
     return 0;
 }
