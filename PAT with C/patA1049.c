bcfcd3562112c92e6db81be77d16adef79157f69
4kingRAS <4king4ever@gmail.com>
2019-08-14 23:25:15 +0800

    Create patA1049.c

---
+++ PAT with C/patA1049.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// ** PAT A 1049 counting ones
+
+// The task is simple: given any positive integer N, you are supposed to count the 
+// total number of 1's in the decimal form of the integers from 1 to N. 
+// For example, given N being 12, there are five 1's in 1, 10, 11, and 12.
+
+// Input Specification:
+// Each input file contains one test case which gives the positive N (≤2^30)
+
+// Output Specification:
+// For each test case, print the number of 1's in one line.
+
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        n = n / 10;
+    }
+    
+
+    return 0;
+}
